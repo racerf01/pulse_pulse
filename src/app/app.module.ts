26173f8ae998f5aec2b779b0b67bc872863dd51d
racerf01 <racerf01@mail.ru>
2025-04-06 04:33:45 +0200

    Added: styling, sidebar styling and some knobs styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     WebglComponent,
-    SidebarComponent
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
